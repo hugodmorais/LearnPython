516e81d2e1026cbae4c3c3f2b03d97efbb54364a
Hugo Morais <hugomorais@aquaporservicos.pt>
2020-04-16 23:25:30 +0100

    basic commands python

--- The_basics/basics.py
+++ The_basics/basics.py
@@ -1,3 +1,15 @@
 import datetime
 print("Hello World!")
-print(datetime.datetime.now())
+mynow = datetime.datetime.now()
+
+print(mynow)
+
+x = 10
+y = "10"
+z = 10.1
+
+sum1 = x + x
+sum2 = y + y
+
+print(sum1, sum2)
+print(type(x), type(y), type(z))
