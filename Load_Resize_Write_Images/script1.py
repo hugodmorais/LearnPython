f1bc2c917abf40eb2a69e53c6758167d82d103b6
hugodmorais <hugodanielmorais@gmail.com>
2020-06-18 23:18:32 +0100

    read and resize multiple images

--- Load_Resize_Write_Images/script1.py
+++ Load_Resize_Write_Images/script1.py
@@ -1,9 +1,12 @@
 import cv2
+import glob
 
-img=cv2.imread("galaxy.jpg",0)
+images=glob.glob("*.jpg")
 
-resized_image=cv2.resize(img,(int(img.shape[1]/2),int(img.shape[0]/2)))
-cv2.imshow("Galaxy",resized_image)
-cv2.imwrite("Galaxy_resized.jpg",resized_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+for image in images:
+    img=cv2.imread(image,0)
+    re=cv2.resize(img,(100,100))
+    cv2.imshow("Hey",re)
+    cv2.waitKey(500)
+    cv2.destroyAllWindows()
+    cv2.imwrite("resized_"+image,re)
