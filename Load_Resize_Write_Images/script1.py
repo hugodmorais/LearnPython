e058bc9ccf8cec550dca119e5573cb3291e0d51c
hugodmorais <hugodanielmorais@gmail.com>
2020-06-18 21:28:33 +0100

    read image, resize image

---
+++ Load_Resize_Write_Images/script1.py
@@ -0,0 +1,9 @@
+import cv2
+
+img=cv2.imread("galaxy.jpg",0)
+
+resized_image=cv2.resize(img,(int(img.shape[1]/2),int(img.shape[0]/2)))
+cv2.imshow("Galaxy",resized_image)
+cv2.imwrite("Galaxy_resized.jpg",resized_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
