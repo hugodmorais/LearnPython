8a5ff5747b51e4e9f9b0517c24103017fa54fd1a
hugodmorais <hugodanielmorais@gmail.com>
2020-06-20 13:18:06 +0100

    Video capture finish

--- Video_capture/capture.py
+++ Video_capture/capture.py
@@ -2,14 +2,19 @@ import cv2, time
 
 video=cv2.VideoCapture(0)
 
-check, frame = video.read()
+while True:
+  check, frame = video.read()
 
-print(check)
-print(frame)
+  print(check)
+  print(frame)
 
-time.sleep(3)
-cv2.imshow("Capturing",frame)
+  gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+  # time.sleep(3)
+  cv2.imshow("Capturing",gray)
 
-cv2.waitKey(0)
+  key=cv2.waitKey(1)
+
+  if key==ord('q'):
+    break
 video.release()
 cv2.destroyAllWindows
