772d9aa6b7bf64dfb4adf46d6238098b4f9644ed
hugodmorais <hugodanielmorais@gmail.com>
2020-06-20 12:06:56 +0100

    Video capture cv2

---
+++ Video_capture/capture.py
@@ -0,0 +1,15 @@
+import cv2, time
+
+video=cv2.VideoCapture(0)
+
+check, frame = video.read()
+
+print(check)
+print(frame)
+
+time.sleep(3)
+cv2.imshow("Capturing",frame)
+
+cv2.waitKey(0)
+video.release()
+cv2.destroyAllWindows
